8b9d9b067f14909307b1fd299ec0da0d9221b4ba
tosmansk <38297885+tosmansk@users.noreply.github.com>
2018-05-02 22:03:46 +0200

    com
    com

--- test_podzespoly.py
+++ test_podzespoly.py
@@ -30,6 +30,7 @@ class KomputeryClick(unittest.TestCase):
         with open('remote_server.cfg', 'r') as config:
             uri = config.readline()
 
+
         self.driver = webdriver.Remote(command_executor=uri, desired_capabilities=DesiredCapabilities.FIREFOX)
 
         logging_format = '%(levelname)-15s %(asctime)s %(funcName)s %(message)s'
